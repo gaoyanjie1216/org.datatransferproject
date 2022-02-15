134dbc75466096997119bd59b64fd396e5f93ec9
gaoyanjie <gaoyanjie@xiaomi.com>
2022-02-15 16:07:21 +0800

    Initial commit

---
+++ portability-spi-transfer/src/main/java/org/datatransferproject/spi/transfer/idempotentexecutor/IdempotentImportExecutorExtension.java
@@ -0,0 +1,9 @@
+package org.datatransferproject.spi.transfer.idempotentexecutor;
+
+import org.datatransferproject.api.launcher.BootExtension;
+import org.datatransferproject.api.launcher.ExtensionContext;
+import org.datatransferproject.api.launcher.Monitor;
+
+public interface IdempotentImportExecutorExtension extends BootExtension {
+  IdempotentImportExecutor getIdempotentImportExecutor(ExtensionContext extensionContext);
+}
